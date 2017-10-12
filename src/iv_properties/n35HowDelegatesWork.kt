124baa03da7276d842fd47d392766ba3f3535e33
Duy Mac Van <jenthone@gmail.com>
2017-10-12 23:54:40 +0700

    solve properties

--- src/iv_properties/n35HowDelegatesWork.kt
+++ src/iv_properties/n35HowDelegatesWork.kt
@@ -29,8 +29,15 @@ class D {
 class EffectiveDate<R> : ReadWriteProperty<R, MyDate> {
     var timeInMillis: Long? = null
 
-    operator override fun getValue(thisRef: R, property: KProperty<*>): MyDate = todoTask35()
-    operator override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) = todoTask35()
+    operator override fun getValue(thisRef: R, property: KProperty<*>): MyDate {
+        if(timeInMillis == null) {
+            timeInMillis = 0
+        }
+        return timeInMillis!!.toDate()
+    }
+    operator override fun setValue(thisRef: R, property: KProperty<*>, value: MyDate) {
+        this.timeInMillis = value.toMillis()
+    }
 }
 
 fun MyDate.toMillis(): Long {
